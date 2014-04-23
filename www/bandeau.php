ee038be8a1e8acd83d848fdbcc99a77579e96541
Durand <vlad.zdk@gmail.com>
2014-04-23 21:16:01 +0200

    New bandeau

---
+++ www/bandeau.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/drop.css">
+		<link rel="stylesheet" type="text/css" href="css/table.css">
+		<link rel="icon" href="images/favicon2.png" sizes="16x16" />
+   		<link rel="icon" href="images/favicon2.ico" sizes="16x16" />
+   		<link rel="icon" href="images/favicon2.icns" sizes="16x16" />
+		<title>Intranet association Revivre</title>
+	</head>
+	<body>
+		<div id="wrapper" background-color="white">
+			<div id="entete">
+				<a href="home.php"><img src="images/logo.jpg"></a>
+				<div id="navi">
+				<nav>
+					<ul class="bandeau">
+						<li id="champs"><a href="home.php">Accueil</a></li>
+						<li id="champs"><a href="#">Fournisseurs</a></li>
+						<li id="champs"><a href="viewClients.php">Clients</a>
+							<ul class="subMenu">
+                       			<li><a href="addClient.php">Ajouter<br>client</a></li>
+								<li><a href="viewClients.php">Liste<br>clients
+								</a></li>
+                			</ul>
+						</li>
+
+						<li id="champs"><a href="#">Chantiers</a>
+							<ul class="subMenu">
+                       			<li><a href="#">Ajouter<br>chantier</a></li>
+								<li><a href="#">Chantiers en cours</a></li>
+								<li><a href="#">Archive chantiers</a></li>
+                			</ul>
+						</li>
+						<li id="champs"><a href="#">Locations</a>
+                			<ul class="subMenu">
+                       			<li><a href="#">Véhicules &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+                        		<li><a href="#">Appartement &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+                			</ul>
+        				</li>
+						<li id="champs">
+							<a href="#">Encadrants </a>
+						</li>
+						<li id="champs">
+							<a href="#">Outillages </a>
+						</li>
+					</ul>	
+				</nav>
+			</div>
+			</div>
+		</div>
