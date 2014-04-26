396243c7842b111b2d58746ea1e07a4d66f0645e
Durand <vlad.zdk@gmail.com>
2014-04-26 15:50:48 +0200

    Client Edition

--- www/editClient.php
+++ www/editClient.php
@@ -2,11 +2,24 @@
 		include('bandeau.php');
 		?>
 		<div id="corps">
+			<?php
+	if($db = MySQLi_connect("localhost","Kepha",'pfudor', 'Revivre', 0, '/media/sds1/home/alx22/private/mysql/socket'))
+		echo '';
+	else
+		echo 'Erreur';
+
+  $num=$_POST["NumC"];
+
+  $reponse = mysqli_query($db, "SELECT * FROM Clients cl JOIN Personnes pe ON cl.PER_Num=pe.PER_Num WHERE CLI_NumClient='$num'");
+  $donnees = mysqli_fetch_assoc($reponse);
+  
+	?>
 			<div id="labelT">     
 				<label>Ajouter un Client</label>
 			</div>
 			<br>
-			<form method="post" action="clientpst.php" name="Client" formtype="1" colvalue="2">
+			<form method="post" action="clientpre.php" name="Client" formtype="1" colvalue="2">
+				<input type="hidden" name="NumC" value="<?php echo $donnees['CLI_NumClient']; ?>">
 				<div style="overflow:auto;">
 					<table align="left">
 						<td style="vertical-align:top;">
@@ -16,7 +29,7 @@
 										<label>Nom :</label>
 									</td>
 									<td>
-										<input id="Nom" required maxlength="255" name="Nom" type="text" class="inputC" pattern="^[A-Z].*" title="Majuscule en début obligatoire" autofocus="autofocus"> 
+										<input id="Nom" required maxlength="255" name="Nom" type="text" class="inputC" pattern="^[A-Z].*" title="Majuscule en début obligatoire" value="<?php echo $donnees['PER_Nom']; ?>"> 
 									</td>
 								</tr>
 								<tr id="Client-Prenom">
@@ -24,7 +37,7 @@
 										<label>Prenom :</label>
 									</td>
 									<td>
-										<input id="Prenom" required maxlength="255" name="Prenom" type="text" class="inputC" pattern="^[A-Z].*" title="Majuscule en début obligatoire"> 
+										<input id="Prenom" required maxlength="255" name="Prenom" type="text" class="inputC" pattern="^[A-Z].*" title="Majuscule en début obligatoire" value="<?php echo $donnees['PER_Prenom']; ?>"> 
 									</td>
 
 								</tr>
@@ -33,7 +46,7 @@
 										<label>Telephone Fixe :</label>
 									</td>
 									<td>
-										<input id="Tel_Fixe" maxlength="255" name="Tel_Fixe" type="text" fieldtype="1" class="inputC" pattern="^[0-9][0-9](?:[\/_:-\s]?\d\d){4}$"> 
+										<input id="Tel_Fixe" maxlength="255" name="Tel_Fixe" type="text" fieldtype="1" class="inputC" pattern="^[0-9][0-9](?:[\/_:-\s]?\d\d){4}$" value="<?php echo $donnees['PER_TelFixe']; ?>"> 
 									</td>
 								</tr>
 								<tr id="Client-Portable">
@@ -41,7 +54,7 @@
 										<label>Telephone Portable :</label>
 									</td>
 									<td>
-										<input id="Portable" maxlength="255" name="Portable" type="text" class="inputC" pattern="^[0-9][0-9](?:[\/_:-\s]?\d\d){4}$"> 
+										<input id="Portable" maxlength="255" name="Portable" type="text" class="inputC" pattern="^[0-9][0-9](?:[\/_:-\s]?\d\d){4}$" value="<?php echo $donnees['PER_TelPort']; ?>"> 
 									</td>
 								</tr>
 								<tr id="Client-Fax">
@@ -49,7 +62,7 @@
 										<label>Fax :</label>
 									</td>
 									<td>
-										<input id="Fax" maxlength="255" name="Fax" type="text" fieldtype="1" class="inputC" pattern="^[0-9][0-9](?:[\/_:-\s]?\d\d){4}$"> 
+										<input id="Fax" maxlength="255" name="Fax" type="text" fieldtype="1" class="inputC" pattern="^[0-9][0-9](?:[\/_:-\s]?\d\d){4}$" value="<?php echo $donnees['PER_Fax']; ?>"> 
 									</td>
 								</tr>
 							</table>
@@ -61,7 +74,7 @@
 										<label>Email :</label>
 									</td>
 									<td>
-										<input id="Email" maxlength="255" name="Email" type="text" class="inputC" title="exemple@exemple.com" pattern="^[a-zA-Z0-9\._-]+@[a-zA-Z0-9\._-]+\.[a-z]{2,3}$"> 
+										<input id="Email" maxlength="255" name="Email" type="text" class="inputC" title="exemple@exemple.com" pattern="^[a-zA-Z0-9\._-]+@[a-zA-Z0-9\._-]+\.[a-z]{2,3}$" value="<?php echo $donnees['PER_Email']; ?>"> 
 									</td>
 								</tr>
 								<tr id="Client-Adresse">
@@ -69,7 +82,7 @@
 										<label>Adresse :</label>
 									</td>
 									<td>
-										<input id="Adresse" required maxlength="255" name="Adresse" type="text" fieldtype="1" class="inputC" delugetype="STRING">
+										<input id="Adresse" required maxlength="255" name="Adresse" type="text" fieldtype="1" class="inputC" delugetype="STRING" value="<?php echo $donnees['PER_Adresse']; ?>">
 									</td>
 								</tr>
 								<tr id="Client-Code_Postal">
@@ -77,7 +90,7 @@
 										<label>Code Postal :</label>
 									</td>
 									<td>
-										<input id="Code_Postal" required name="Code_Postal" type="text" title="" fieldtype="5" style="width:100px;background-color:#cde5f7;" delugetype="BIGINT" maxlength="5">
+										<input id="Code_Postal" required name="Code_Postal" type="text" title="" fieldtype="5" style="width:100px;background-color:#cde5f7;" delugetype="BIGINT" maxlength="5" value="<?php echo $donnees['PER_CodePostal']; ?>">
 									</td>
 								</tr>
 								<tr id="Client-Ville">
@@ -85,7 +98,7 @@
 										<label>Ville :</label>
 									</td>
 									<td>
-										<input id="Ville" required maxlength="255" name="Ville" type="text" fieldtype="1" class="inputC" delugetype="STRING">
+										<input id="Ville" required maxlength="255" name="Ville" type="text" fieldtype="1" class="inputC" delugetype="STRING" value="<?php echo $donnees['PER_Ville']; ?>">
 									</td>
 								</tr>
 								<tr id="Client-Structure">
@@ -93,7 +106,7 @@
 										<label>Structure :</label>
 									</td>
 									<td>
-										<input id="Struct" maxlength="255" name="Struct" type="text" fieldtype="1" class="inputC" delugetype="STRING">
+										<input id="Struct" maxlength="255" name="Struct" type="text" fieldtype="1" class="inputC" delugetype="STRING" value="<?php echo $donnees['CLI_Structure']; ?>">
 									</td>
 								</tr>
 							</table>
@@ -104,7 +117,7 @@
 					<tr>
 						<td>
 							<span>
-								<input name="submit" type="submit" value="Ajouter" class="buttonC">&nbsp;&nbsp; 
+								<input name="submit" type="submit" value="Valider" class="buttonC">&nbsp;&nbsp; 
 								<input name="reset" type="reset" value="RAZ" class="buttonC">
 							</span>
 						</td>
