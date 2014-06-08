66d5b5a1b00ccd5f2ebfe191bdff12374ea090ba
Durand <vlad.zdk@gmail.com>
2014-06-08 20:45:57 +0200

    Ajout Chantier

---
+++ v2.0/addWork.php
@@ -0,0 +1,134 @@
+		<?php  
+		include('bandeau.php');
+		?>
+		<div id="corps">
+			<div id="labelT">     
+				<label>Ajouter un Chantier</label>
+			</div>
+			<br>
+			<form method="post" action="chantierPost.php" name="Chantier" formtype="1" colvalue="2">
+				<div id="labelCat">
+					<table align="center">
+						<tr>
+							<td style="text-align: left; width: 150px;">
+								<label>Client</label>
+							</td>
+							<td>
+								<div class="selectType">
+		          					<select name="Client">
+		          			<?php
+
+	if($db = MySQLi_connect("localhost","Kepha",'pfudor', 'Revivre', 0, '/media/sds1/home/alx22/private/mysql/socket'))
+		echo '';
+	else
+		echo 'Erreur';
+	$i = 2;
+	$reponse = mysqli_query($db, "SELECT * FROM Clients cl JOIN Personnes pe ON cl.PER_Num=pe.PER_Num ORDER BY PER_Nom");
+	while ($donnees = mysqli_fetch_assoc($reponse))
+	{
+		?>
+				        				<option value="<?php echo $donnees['CLI_NumClient']; ?>"><?php echo $donnees['PER_Nom']; ?></option>
+				        	<?php
+	$i++;
+	}
+	mysqli_free_result($reponse);
+	?>									
+				    				</select>
+				    			</div>
+				    		</td>
+						</tr>
+					</table>
+				</div>
+				<br/>
+				<div style="overflow:auto;">
+					<table align="left">
+						<td style="vertical-align:top;">
+							<table id="leftT" colcount="0" cellpadding="10">
+								<tr id="Chantier_Num">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Numéro de Devis :</label>
+									</td>
+									<td>
+										<input id="Num" required maxlength="255" name="Num" type="text" class="inputC"> 
+									</td>
+								</tr>
+								<tr id="Contact-Nom">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Nom du Chantier :</label>
+									</td>
+									<td>
+										<input id="Nom" required maxlength="255" name="Nom" type="text" class="inputC" autofocus="autofocus"> 
+									</td>
+								</tr>
+								<tr id="Contact-Prenom">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Date de Début :</label>
+									</td>
+									<td>
+										<input id="Debut" maxlength="255" name="Debut" type="date" class="inputC"> 
+									</td>
+
+								</tr>
+								<tr id="Contact-Echeance">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Echéance :</label>
+									</td>
+									<td>
+										<input id="Fin_Max" maxlength="255" name="Fin_max" type="date" class="inputC"> 
+									</td>
+								</tr>
+							</table>
+						</td>
+						<td style="vertical-align:top;">
+							<table id="rightT" colcount="1" cellpadding="10">
+								<tr id="Contact-Tel_Fixe">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Date de Fin Prévue :</label>
+									</td>
+									<td>
+										<input id="Fin_Prev" maxlength="255" name="Fin_Prev" type="date" class="inputC"> 
+									</td>
+								</tr>
+								<tr id="Contact-Portable">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Montant Prévu :</label>
+									</td>
+									<td>
+										<input id="Montant_Prev" maxlength="255" name="Montant_Prev" type="text" class="inputC"> 
+									</td>
+								</tr>
+								<tr id="Contact-Fax">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Achats Prévus :</label>
+									</td>
+									<td>
+										<input id="Achats_Prev" maxlength="255" name="Achats_Prev" type="text" class="inputC"> 
+									</td>
+								</tr>
+								<tr id="Contact-Fonction">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Heures Prévues :</label>
+									</td>
+									<td>
+										<input id="Heures_Prev" maxlength="255" name="Heures_Prev" type="text" class="inputC">
+									</td>
+								</tr>
+							</table>
+						</td>
+					</table>
+				</div>
+				<table id="downT">
+					<tr>
+						<td>
+							<span>
+								<input name="submit" type="submit" value="Ajouter" class="buttonC">&nbsp;&nbsp; 
+								<input name="reset" type="reset" value="Annuler" class="buttonC">
+							</span>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+		<?php  
+		include('footer.php');
+		?>
---
+++ v2.0/chantierPost.php
@@ -0,0 +1,108 @@
+<?php  
+    include('bandeau.php');
+    ?>
+    <div id="corps">
+<?php
+	if($db = MySQLi_connect("localhost","Kepha",'pfudor', 'Revivre', 0, '/media/sds1/home/alx22/private/mysql/socket'))
+		echo '';
+	else
+		echo 'Erreur';
+
+  $client=addslashes($_POST["Client"]);
+  $nom=addslashes($_POST["Nom"]);
+  $num=addslashes($_POST["Num"]);
+  $ddebut=addslashes($_POST["Debut"]);
+  $dfinp=addslashes($_POST["Fin_Prev"]);
+  $montantp=addslashes($_POST["Montant_Prev"]);
+  $achatp=addslashes($_POST["Achats_Prev"]);
+  $heurep=addslashes($_POST["Heures_Prev"]);
+  $echeance=addslashes($_POST["Fin_Max"]);
+
+	$query = "INSERT INTO Chantiers (CHA_DateDebut, CHA_DateFinPrevue, CHA_Intitule, CHA_Echeance, CHA_MontantPrev, CHA_AchatsPrev, CHA_HeuresPrev, CHA_Id) VALUES ('$ddebut', '$dfinp', '$nom', '$echeance', '$montantp', '$achatp', '$heurep', '$num')";
+
+  $sql = mysqli_query($db, $query);
+  $errr=mysqli_error($db);
+
+    if($sql){
+      $numberQuery = mysqli_query($db, "SELECT * FROM Chantiers WHERE CHA_Id='$num'");
+      $numberRep = mysqli_fetch_assoc($numberQuery);
+      $realNumber = $numberRep['CHA_NumDevis'];
+
+      $query2 = "INSERT INTO Commanditer (CLI_NumClient, CHA_NumDevis) VALUES ('$client', '$realNumber')";
+      
+      $sql2 = mysqli_query($db, $query2);
+      $errr2 = mysqli_error($db);
+
+      if($sql2){
+        echo '<div id="good">     
+            <label>Chantier ajouté avec succès</label>
+            </div>';
+            $reponse = mysqli_query($db, "SELECT * FROM Chantiers ch JOIN Commanditer co ON co.CHA_NumDevis=ch.CHA_NumDevis JOIN Clients cl ON cl.CLI_NumClient=co.CLI_NumClient JOIN Personnes pe ON cl.PER_Num=pe.PER_Num WHERE ch.CHA_NumDevis='$realNumber'");
+            
+            $donnees = mysqli_fetch_assoc($reponse);
+	?>
+  <br>
+  <table>
+  <td>
+  <table cellpadding="10" class="submitClients">
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Numéro de Devis:</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['CHA_Id']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Nom du Chantier:</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['CHA_Intitule']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Date de Début:</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['CHA_DateDebut']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Echéance:</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['CHA_Echeance']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Montant Prévu:</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['CHA_MontantPrev']; ?></td>
+    </tr>
+  </table>
+</td>
+<td>
+  <table cellpadding="10" class="submitClients">
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Nom Client:</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['PER_Nom']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Tél Fixe :</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['PER_TelFixe']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Email :</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['PER_Email']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Adresse :</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['PER_Adresse']; ?></td>
+    </tr>
+    <tr>
+      <th style="text-align: left; width: 150px; white-space: normal;">Ville :</th>
+      <td style="text-align: center; width: 200px;"><?php echo $donnees['PER_Ville']; ?>, <?php echo $donnees['PER_CodePostal']; ?></td>
+    </tr>
+  </table>
+</td>
+</table>
+  <?php
+      }
+      else{
+        echo '<div id="bad">     
+              <label>Le chantier n\'a pas pu être ajouté</label>
+              </div>';
+            }
+            mysqli_free_result($reponse);
+    }
+  ?>
+  </div>
+  <?php  
+    include('footer.php');
+    ?>
