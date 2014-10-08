20863ce9faaa7a32985f4b87be17a3d7dbf45e81
Durand <vlad.zdk@gmail.com>
2014-10-08 11:55:15 +0200

    fix repartition reallity

--- repartitionTaches.c
+++ repartitionTaches.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(void) {
 	int i, j;
+	int statV = 0;
+	int statP = 0;
+	int statA = 0;
+	int statJ = 0;
+	
 
 	const char *assignment[26];
 	assignment[0] = "Liste personne";
@@ -33,20 +39,34 @@ int main(void) {
 	assignment[24] = "vues SQL";
 	assignment[25] = "Importation donnees";
 
-	const char *guys[4];
+	const char *guys[6];
 	guys[0] = "Valentin D";
 	guys[1] = "Alexandre F";
 	guys[2] = "Pierre F";
 	guys[3] = "Jeremie LB";
+	guys[4] = "Valentin D";
+	guys[5] = "Pierre F";
 
 	srand(time(NULL));
 
 	printf("-Repartition des taches random-\n\n");
 
 	for(i=0; i<26; i++){
-		j = rand()%(4);
+		j = rand()%(6);
 		printf("%d : %s ==> %s\n",i+1,assignment[i], guys[j]);
+		if (strcmp(guys[j], "Valentin D")==0){
+			statV++;
+		}
+		if (strcmp(guys[j], "Jeremie LB")==0){
+			statJ++;
+		}
+		if (strcmp(guys[j], "Pierre F")==0){
+			statP++;
+		}
+		if (strcmp(guys[j], "Alexandre F")==0){
+			statA++;
+		}
 	}
-	printf("\n");
+	printf("\nValentin = %d\nPierre = %d\nAlexandre = %d\nJeremie = %d\n", statV, statP, statA, statJ);
 	return 0;
 }
