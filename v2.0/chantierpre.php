59818eca9d6f4bf9b328001780ef719768072531
Pierre Friboulet <kepha76@voila.fr>
2014-11-17 00:06:12 +0100

    New editChantier
    Need fix

--- v2.0/chantierpre.php
+++ v2.0/chantierpre.php
@@ -6,32 +6,68 @@
     <div id="corps">
 <?php
 // Modification d un chantier
-  $id=$_POST["NumC"];		
+  $id=$_POST["NumC"];
+  $client=$_POST["Client"];
   $nom=addslashes(mysqli_real_escape_string($db, formatLOW($_POST["Nom"])));
+  $add=addslashes(mysqli_real_escape_string($db, formatLOW($_POST["Add"])));
   $ddebut=addslashes($_POST["Debut"]);
   $montantp=addslashes($_POST["Montant_Prev"]);
   $achatp=addslashes($_POST["Achats_Prev"]);
   $heurep=addslashes($_POST["Heures_Prev"]);
   $echeance=addslashes($_POST["Fin_Max"]);
+  $resp=addslashes($_POST["Resp"]);
+  $tva=addslashes($_POST["tva"]);
+  $txh=addslashes($_POST["TxH"]);
   date_default_timezone_set('Europe/Paris');
+  $dateNow = date('Y-m-d H:i:s', time());
   
-  $query = "UPDATE Chantiers SET CHA_Intitule = '$nom', CHA_DateDebut = '$ddebut', CHA_MontantPrev	 = '$montantp', CHA_AchatsPrev = '$achatp', CHA_HeuresPrev = '$heurep', CHA_Echeance = '$echeance' WHERE Chantiers.CHA_NumDevis = '$id'";
+  $query = "UPDATE Chantiers SET CHA_Intitule = '$nom', CHA_Adresse = '$add', CHA_DateDebut = '$ddebut', CHA_Echeance = '$echeance', CHA_MontantPrev = '$montantp', CHA_AchatsPrev = '$achatp', CHA_HeuresPrev = '$heurep', CHA_TVA = '$tva', CHA_TxHoraire = '$txh' WHERE CHA_NumDevis = '$id'";
   
   $sql = mysqli_query($db, $query);
-  $errr=mysqli_error($db);
+  $errr= mysqli_error($db);
   
   if($sql){
-        echo '
-		<div id="good">     
-            <label>Chantier modifie avec succes</label>
-            </div>
-			';
-  }
-  else{
-    echo '<div id="bad">     
-          <label>Le chantier n a pas pu etre modifie</label>
-          </div>';
-  }
+	$query2 = "UPDATE Commanditer SET 
+	CLI_NumClient = '$client'
+	WHERE CHA_NumDevis = '$id'";
+	  
+	$sql2 = mysqli_query($db, $query2);
+	$errr = mysqli_error($db);
+	
+		if($sql2){
+		
+			$query3 = "UPDATE Encadrer SET 
+			SAL_NumSalarie = '$resp'
+			WHERE CHA_NumDevis = '$id'";
+	  
+			$sql3 = mysqli_query($db, $query3);
+			$errr = mysqli_error($db);
+			
+			if($sql3){
+				echo '
+				<div id="good">     
+				<label>Chantier modifie avec succes</label>
+				</div>
+				';
+			}
+			else{
+				echo '<div id="bad">     
+				<label>Le chantier n a pas pu etre modifie3</label>
+				</div>';
+			}
+			
+		}
+		else{
+			echo '<div id="bad">     
+			<label>Le chantier n a pas pu etre modifie2</label>
+			</div>';
+		}	
+	}
+	else{
+		echo '<div id="bad">     
+		<label>Le chantier n a pas pu etre modifie1</label>
+        </div>';
+	}
 ?>
 <?php
   $Workers = array(" ");
--- v2.0/editChantier.php
+++ v2.0/editChantier.php
@@ -1,4 +1,4 @@
-﻿<?php  
+<?php  
     include('bandeau.php');
 ?>
     <div id="corps">
@@ -11,11 +11,82 @@
 				<label>Modifier Chantier</label>
 			</div>
 			<br>
-			<form method="post" action="chantierpre.php" name="Client" formtype="1" colvalue="2">
-				<input type="hidden" name="NumC" value="<?php echo $donnees['CHA_NumDevis']; ?>">
-				<div style="overflow:auto;">
+			<form method="post" action="chantierpre.php" name="Chantier" formtype="1" colvalue="2">
+				<div id="labelCat">
+					<table align="center">
+						<tr>
+							<td style="text-align: left; width: 150px;">
+								<label>Client</label>
+							</td>
+							<td>
+								<div class="selectType2">
+		          					<select name="Client">									
+                                        <optgroup label="Client actuel">
+										<?php
+											$reponse = mysqli_query($db, "select * from ChantierClient where CNumDevis ='$num' limit 1");
+											$donnees = mysqli_fetch_assoc($reponse);
+										?>
+											<option value="<?php echo $donnees['CLI_NumClient']; ?>"><?php echo formatUP($donnees['Client'])." ".formatLOW($donnees['ClientP']); ?></option>
+										</optgroup>
+										
+                                        <optgroup label="Particuliers">
+<?php
+	$i = 2;
+	$reponse = mysqli_query($db, "SELECT * FROM Clients cl JOIN Personnes pe ON cl.PER_Num=pe.PER_Num WHERE CLI_Structure IS NULL ORDER BY PER_Nom");
+	while ($donnees = mysqli_fetch_assoc($reponse))
+	{
+?>
+				        				<option value="<?php echo $donnees['CLI_NumClient']; ?>"><?php
+                                            if(!empty($donnees['PER_Nom'])){
+                                                echo formatUP($donnees['PER_Nom']);
+                                                if(!empty($donnees['PER_Prenom'])){
+                                                    echo " ".formatLOW($donnees['PER_Prenom']);
+                                                }
+                                            }
+                                            ?>
+                                        </option>
+<?php
+		$i++;
+	}
+	mysqli_free_result($reponse);
+?>									    </optgroup>
+                                        <optgroup label="Structures">
+                                            <?php
+                                            $i = 2;
+                                            $reponse = mysqli_query($db, "SELECT * FROM Clients cl JOIN Personnes pe ON cl.PER_Num=pe.PER_Num WHERE CLI_Structure IS NOT NULL ORDER BY CLI_Structure");
+                                            while ($donnees = mysqli_fetch_assoc($reponse))
+                                            {
+                                                ?>
+                                                <option value="<?php echo $donnees['CLI_NumClient']; ?>"><?php
+                                                    echo formatUP($donnees['CLI_Structure']);
+                                                    if(!empty($donnees['PER_Nom'])){
+                                                        echo " (".formatUP($donnees['PER_Nom']);
+                                                        if(!empty($donnees['PER_Prenom'])){
+                                                            echo " ".formatLOW($donnees['PER_Prenom']);
+                                                        }
+                                                        echo ")";
+                                                    }
+                                                    ?></option>
+                                                <?php
+                                                $i++;
+                                            }
+                                            mysqli_free_result($reponse);
+                                            ?>
+                                        </optgroup>
+				    				</select>
+				    			</div>
+				    		</td>
+						</tr>
+					</table>
+				</div>
+				<br/>
+<?php
+  $reponse = mysqli_query($db, "SELECT * FROM Chantiers ch JOIN ChantierClient vcl ON ch.CHA_NumDevis=vcl.CNumDevis LEFT JOIN ChantierResp vre ON ch.CHA_NumDevis=vre.RNumDevis LEFT JOIN ChantierEtat cet ON ch.CHA_NumDevis=NumDevis WHERE ch.CHA_NumDevis='$num' limit 1");
+  $donnees = mysqli_fetch_assoc($reponse);
+?>
+				<div>
 					<table align="left">
-						<td style="vertical-align:top;">
+						<td>
 							<table id="leftT" colcount="0" cellpadding="10">
 								<tr id="Contact-Nom">
 									<td style="text-align: left; width: 150px; white-space: normal;">
@@ -25,20 +96,12 @@
 										<input id="Nom" required maxlength="255" name="Nom" type="text" class="inputC" autofocus="autofocus" value="<?php echo formatLOW($donnees['CHA_Intitule']); ?>"> 
 									</td>
 								</tr>
-								<tr id="Contact-Nom">
-									<td style="text-align: left; width: 150px; white-space: normal;">
-										<label>Adresse :</label>
-									</td>
-									<td>
-										<input id="Adresse" required maxlength="255" name="Adresse" type="text" class="inputC" autofocus="autofocus" value="<?php echo formatLOW($donnees['CHA_Adresse']); ?>"> 
-									</td>
-								</tr>
 								<tr id="Contact-Prenom">
 									<td style="text-align: left; width: 150px; white-space: normal;">
 										<label>Date de Début :</label>
 									</td>
 									<td>
-										<input id="Debut" required maxlength="255" name="Debut" type="date" class="inputC" value="<?php echo ($donnees['CHA_DateDebut']); ?>"> 
+										<input id="Debut" required maxlength="255" name="Debut" type="date" class="inputC" value="<?php echo formatLOW($donnees['CHA_DateDebut']); ?>"> 
 									</td>
 
 								</tr>
@@ -50,16 +113,32 @@
 										<input id="Fin_Max" maxlength="255" name="Fin_Max" type="date" class="inputC" value="<?php echo ($donnees['CHA_Echeance']); ?>"> 
 									</td>
 								</tr>
+                                <tr id="Chantier_Ad">
+                                    <td style="text-align: left; width: 150px; white-space: normal;">
+                                        <label>Adresse Chantier :</label>
+                                    </td>
+                                    <td>
+                                        <input id="Add" required maxlength="255" name="Add" type="text" class="inputC" value="<?php echo ($donnees['CHA_Adresse']); ?>">
+                                    </td>
+                                </tr>
+                                <tr id="Chantier_Tva">
+                                    <td style="text-align: left; width: 150px; white-space: normal;">
+                                        <label>TVA :</label>
+                                    </td>
+                                    <td>
+                                        <input id="tva" required name="tva" type="number" step="0.01" class="inputC" value="<?php echo ($donnees['CHA_TVA']); ?>">
+                                    </td>
+                                </tr>
 							</table>
 						</td>
-						<td style="vertical-align:top;">
+						<td>
 							<table id="rightT" colcount="1" cellpadding="10">
 								<tr id="Contact-Portable">
 									<td style="text-align: left; width: 150px; white-space: normal;">
 										<label>Montant :</label>
 									</td>
 									<td>
-										<input id="Montant_Prev" maxlength="255" name="Montant_Prev" type="text" class="inputC" value="<?php echo $donnees['CHA_MontantPrev']; ?>"> 
+										<input id="Montant_Prev" maxlength="255" name="Montant_Prev" type="text" class="inputC" placeholder="&euro;" value="<?php echo $donnees['CHA_MontantPrev']; ?>"> 
 									</td>
 								</tr>
 								<tr id="Contact-Fax">
@@ -78,6 +157,59 @@
 										<input id="Heures_Prev" maxlength="255" name="Heures_Prev" type="text" class="inputC" value="<?php echo $donnees['CHA_HeuresPrev']; ?>">
 									</td>
 								</tr>
+								<tr id="Chantier_Resp">
+									<td style="text-align: left; width: 150px; white-space: normal;">
+										<label>Responsable :</label>
+									</td>
+									<td>
+										<div class="selectType">
+										            <select name="Resp">
+														<optgroup label="Responsable actuel">
+														<?php
+															$reponse = mysqli_query($db, "select * from ChantierResp where RNumDevis ='$num' limit 1");
+															$donnees = mysqli_fetch_assoc($reponse);
+														?>
+															<option value="<?php echo $donnees['NumSal']; ?>"><?php echo formatUP($donnees['Resp']); ?> <?php echo formatLOW($donnees['RespP']); ?></option>
+														</optgroup>
+<?php
+    $reponseType = mysqli_query($db, "SELECT * FROM Type");
+    while ($donneesType = mysqli_fetch_assoc($reponseType))
+    {
+        $typeNOM = $donneesType['TYP_Nom'];
+        $typeID = $donneesType['TYP_Id'];
+?>
+                                                        <optgroup label="<?php echo $typeNOM; ?>">
+<?php
+        $reponse = mysqli_query($db, "SELECT * FROM Salaries cl JOIN Personnes pe ON cl.PER_Num=pe.PER_Num WHERE TYP_Id=$typeID ORDER BY PER_Nom");
+        while ($donnees = mysqli_fetch_assoc($reponse))
+        {
+?>
+														<option value="<?php echo $donnees['SAL_NumSalarie']; ?>"><?php echo formatUP($donnees['PER_Nom']); ?> <?php echo formatLOW($donnees['PER_Prenom']); ?></option>
+<?php
+        }
+        mysqli_free_result($reponse);
+?>
+                                                        </optgroup>
+<?php
+    }
+mysqli_free_result($reponseType);
+?>
+										            </select>
+										          </div>
+									</td>
+								</tr>
+<?php
+  $reponse = mysqli_query($db, "SELECT * FROM Chantiers ch JOIN ChantierClient vcl ON ch.CHA_NumDevis=vcl.CNumDevis LEFT JOIN ChantierResp vre ON ch.CHA_NumDevis=vre.RNumDevis LEFT JOIN ChantierEtat cet ON ch.CHA_NumDevis=NumDevis WHERE ch.CHA_NumDevis='$num' limit 1");
+  $donnees = mysqli_fetch_assoc($reponse);
+?>
+                                <tr id="Chantier_TxH">
+                                    <td style="text-align: left; width: 150px; white-space: normal;">
+                                        <label>Taux Horaire :</label>
+                                    </td>
+                                    <td>
+                                        <input id="TxH" required step="0.01" name="TxH" type="number" class="inputC" value="<?php echo $donnees['CHA_TxHoraire']; ?>">
+                                    </td>
+                                </tr>
 							</table>
 						</td>
 					</table>
@@ -101,8 +233,8 @@
 						</td>
 					</tr>
 				</table>
-			
-		</div>
+			</form>
+	</div>
 <?php  
 	include('footer.php');
 ?>
