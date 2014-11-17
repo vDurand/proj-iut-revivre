d8216875485ceb0241aeee488786141360d04226
Pierre Friboulet <kepha76@voila.fr>
2014-11-17 18:40:06 +0100

    printer
    print a lot baby !

--- v2.0/printer.php
+++ v2.0/printer.php
@@ -1,4 +1,16 @@
 <?php
+	include('assets.php');
+	$db = revivre();
+	$id=$_POST["NumC"];
+	$totAchat=0;
+	$reponse5 = mysqli_query($db, "SELECT ROUND(SUM(ACH_MONTANT), 2) as totAchat FROM Acheter WHERE CHA_NumDevis='$id' GROUP BY CHA_NumDevis");
+	$donnees5 = mysqli_fetch_assoc($reponse5);
+	$reponse4 = mysqli_query($db, "SELECT SUM(TRA_Duree) as total FROM TempsTravail ttps WHERE ttps.CHA_NumDevis='$id' GROUP BY CHA_NumDevis");
+    $donnees4 = mysqli_fetch_assoc($reponse4);
+	$reponse = mysqli_query($db, "SELECT * FROM ChantierMax WHERE CHA_NumDevis='$id' limit 1");
+	$donnees = mysqli_fetch_assoc($reponse);
+	$resteHeure = $donnees['CHA_HeuresPrev']-$donnees4['total'];
+	$resteAchat = $donnees['CHA_AchatsPrev']-$donnees5['totAchat'];
 	$content = "
 <page>
 	<h1><img src=\"images/logoBW.jpg\"> Bordereau de livraison</h1>
@@ -8,23 +20,23 @@
 	<table>
 		<tr>
 			<th>Nom : </th>
-			<td>???</td>
+			<td>".formatUP($donnees['Client'])."</td>
 		</tr>
 		<tr>
 			<th>Prénom : </th>
-			<td>???</td>
+			<td>".formatLOW($donnees['ClientP'])."</td>
 		</tr>
 		<tr>
 			<th>Adresse : </th>
-			<td>???</td>
+			<td>".formatLOW($donnees['ClientAd'])."</td>
 		</tr>
 		<tr>
 			<th>Ville : </th>
-			<td>???</td>
+			<td>".formatUP($donnees['ClientV'])."</td>
 		</tr>
 		<tr>
 			<th>Code Postal : </th>
-			<td>???</td>
+			<td>".$donnees['ClientCP']."</td>
 		</tr>
 	</table>
 	</fieldset>
@@ -34,51 +46,51 @@
 	<table>
 		<tr>
 			<th>Intitulé : </th>
-			<td>???</td>
+			<td>".formatLOW($donnees['CHA_Intitule'])."</td>
 		</tr>
 		<tr>
 			<th>Adresse : </th>
-			<td>???</td>
+			<td>".formatLOW($donnees['CHA_Adresse'])."</td>
 		</tr>
 		<tr>
 			<th>Date de début : </th>
-			<td>???</td>
+			<td>".dater($donnees['CHA_DateDebut'])."</td>
 		</tr>
 		<tr>
 			<th>Date de fin : </th>
-			<td>???</td>
+			<td>".dater($donnees['CHA_DateFinReel'])."</td>
 		</tr>
 		<tr>
 			<th>Montant : </th>
-			<td>???</td>
+			<td>".$donnees['CHA_MontantPrev']." €</td>
 		</tr>
 		<tr>
 			<th>TVA : </th>
-			<td>???</td>
+			<td>".$donnees['CHA_TVA']." %</td>
 		</tr>
 		<tr>
 			<th>Achats prévus : </th>
-			<td>???</td>
+			<td>".$donnees['CHA_AchatsPrev']." €</td>
 		</tr>
 		<tr>
 			<th>Achats effectués : </th>
-			<td>???</td>
+			<td>".$donnees5['totAchat']." €</td>
 		</tr>
 		<tr>
 			<th>Ecart achats : </th>
-			<td>???</td>
+			<td>".$resteAchat." €</td>
 		</tr>
 		<tr>
 			<th>Heures prévues : </th>
-			<td>???</td>
+			<td>".$donnees['CHA_HeuresPrev']."</td>
 		</tr>
 		<tr>
 			<th>Heures effectuées : </th>
-			<td>???</td>
+			<td>".$donnees4['total']."</td>
 		</tr>
 		<tr>
-			<th>Ecart MO : </th>
-			<td>???</td>
+			<th>Ecart heures : </th>
+			<td>".$resteHeure."</td>
 		</tr>
 	</table>
 	</fieldset>
@@ -88,11 +100,11 @@
 	<table>
 		<tr>
 			<th>Nom : </th>
-			<td>???</td>
+			<td>".formatUP($donnees['Resp'])."</td>
 		</tr>
 		<tr>
 			<th>Prénom : </th>
-			<td>???</td>
+			<td>".formatLOW($donnees['RespP'])."</td>
 		</tr>
 	</table>
 	</fieldset>
