5346b849fda03985152c96dc6ac6faf1ee1d6177
Durand <vlad.zdk@gmail.com>
2014-11-26 15:15:53 +0100

    gestion date suivi enc
    il manque les cas ou une des listes est vide (mois ou annee)

--- v2.0/suiviEnc.php
+++ v2.0/suiviEnc.php
@@ -13,6 +13,18 @@
     else{
         $numEnc = 0;
     }
+    if(!empty($_POST['Annee'])){
+        $year = $_POST['Annee'];
+    }
+    else{
+        $year = 0;
+    }
+    if(!empty($_POST['Mois'])){
+        $month = $_POST['Mois'];
+    }
+    else{
+        $month = 0;
+    }
 
     $totMP = 0;
     $totAP = 0;
@@ -26,15 +38,15 @@
     <script src="js/sorttable.js"></script>
     <div id="corps">
         <div id="labelCat" style="padding-bottom: 15px;">
-            <table align="center">
-                <tr>
-                    <td style="text-align: left; width: 150px;">
-                        <label>Encadrant</label>
-                    </td>
-                    <td>
-                        <div class="selectType">
-                            <form method="post" action="suiviEnc.php" name="Encadrant">
-                                <select name="Encad" onchange="this.parentNode.submit()">
+            <form method="post" action="suiviEnc.php" name="Encadrant">
+                <table align="center">
+                    <tr>
+                        <td style="text-align: left; width: 100px;">
+                            <label>Encadrant</label>
+                        </td>
+                        <td>
+                            <div class="selectType">
+                                <select name="Encad" onchange="this.form.submit()">
                                     <option <?php if($numEnc == 0){echo "selected";} ?> value="0">--</option>
                                     <?php
                                     $reponse = mysqli_query($db, "SELECT Distinct SAL_NumSalarie, PER_Nom, PER_Prenom FROM Encadrer JOIN Salaries USING (SAL_NumSalarie) JOIN Personnes USING (PER_Num) ORDER BY PER_Nom");
@@ -47,11 +59,51 @@
                                     mysqli_free_result($reponse);
                                     ?>
                                 </select>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                            </div>
+                        </td>
+                        <td style="text-align: left; width: 100px;">
+                            <label>Année</label>
+                        </td>
+                        <td>
+                            <div class="selectType">
+                                <select name="Annee" onchange="this.form.submit()">
+                                    <option <?php if($year == 0){echo "selected";} ?> value="0">--</option>
+                                    <?php
+                                    for($annee=date("Y")+1;$annee>2000;$annee--)
+                                    {
+                                        ?>
+                                        <option <?php if($annee == $year){echo "selected";} ?> value="<?php echo $annee; ?>"><?php echo $annee; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </td>
+                        <td style="text-align: left; width: 100px;">
+                            <label>Mois</label>
+                        </td>
+                        <td>
+                            <div class="selectType">
+                                <select name="Mois" onchange="this.form.submit()">
+                                    <option <?php if($month == 0){echo "selected";} ?> value="0">--</option>
+                                    <option <?php if($month == 1){echo "selected";} ?> value="1">Janvier</option>
+                                    <option <?php if($month == 2){echo "selected";} ?> value="2">Février</option>
+                                    <option <?php if($month == 3){echo "selected";} ?> value="3">Mars</option>
+                                    <option <?php if($month == 4){echo "selected";} ?> value="4">Avril</option>
+                                    <option <?php if($month == 5){echo "selected";} ?> value="5">Mai</option>
+                                    <option <?php if($month == 6){echo "selected";} ?> value="6">Juin</option>
+                                    <option <?php if($month == 7){echo "selected";} ?> value="7">Juillet</option>
+                                    <option <?php if($month == 8){echo "selected";} ?> value="8">Août</option>
+                                    <option <?php if($month == 9){echo "selected";} ?> value="9">Septembre</option>
+                                    <option <?php if($month == 10){echo "selected";} ?> value="10">Octobre</option>
+                                    <option <?php if($month == 11){echo "selected";} ?> value="11">Novembre</option>
+                                    <option <?php if($month == 12){echo "selected";} ?> value="12">Décembre</option>
+                                </select>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </form>
         </div>
         <table class="tableContact">
             <thead>
@@ -90,25 +142,27 @@
      * COUNT(DISTINCT SAL_NumSalarie) as NbSalarie
      * from Chantiers
      * JOIN TempsTravail USING (CHA_NumDevis)
-     * group by CHA_NumDevis
+     * group by CHA_NumDevis;
      *
      */
 
     if($numEnc == 0){
-        $query = "select CHA_Intitule, CHA_MontantPrev, ac.CHA_AchatsPrev, AchatTot, EcartAch, he.CHA_HeuresPrev, HeureTot, EcartHeure, NbSalarie
+        $query = "select CHA_Intitule, CHA_Echeance, CHA_MontantPrev, ch.CHA_AchatsPrev, AchatTot, EcartAch, he.CHA_HeuresPrev, HeureTot, EcartHeure, NbSalarie
             from Chantiers ch
-            JOIN ChantierAchat ac ON ch.CHA_NumDevis=ac.CHA_NumDevis
+            LEFT JOIN ChantierAchat ac ON ch.CHA_NumDevis=ac.CHA_NumDevis
             JOIN ChantierHeure he ON ch.CHA_NumDevis=he.CHA_NumDevis
             JOIN Encadrer en ON ch.CHA_NumDevis=en.CHA_NumDevis
             ORDER BY CHA_Intitule";
     }
     else{
-        $query = "select CHA_Intitule, CHA_MontantPrev, ac.CHA_AchatsPrev, AchatTot, EcartAch, he.CHA_HeuresPrev, HeureTot, EcartHeure, NbSalarie
+        $query = "select CHA_Intitule, CHA_Echeance, CHA_DateDebut, CHA_DateFinReel, CHA_MontantPrev, ch.CHA_AchatsPrev, AchatTot, EcartAch, he.CHA_HeuresPrev, HeureTot, EcartHeure, NbSalarie
             from Chantiers ch
-            JOIN ChantierAchat ac ON ch.CHA_NumDevis=ac.CHA_NumDevis
+            LEFT JOIN ChantierAchat ac ON ch.CHA_NumDevis=ac.CHA_NumDevis
             JOIN ChantierHeure he ON ch.CHA_NumDevis=he.CHA_NumDevis
             JOIN Encadrer en ON ch.CHA_NumDevis=en.CHA_NumDevis
             WHERE en.SAL_NumSalarie = $numEnc
+            AND (CHA_Echeance >= '".$year."-".$month."-01' OR CHA_DateFinReel >= '".$year."-".$month."-01' OR CHA_DateFinReel is null)
+            AND CHA_DateDebut <= '".$year."-".$month."-31'
             ORDER BY CHA_Intitule";
     }
 
@@ -119,8 +173,8 @@
                 <td><?php echo formatLOW($donnees['CHA_Intitule']); ?></td>
                 <td><?php echo $donnees['CHA_MontantPrev']." €"; $totMP += $donnees['CHA_MontantPrev']; ?></td>
                 <td><?php echo $donnees['CHA_AchatsPrev']." €"; $totAP += $donnees['CHA_AchatsPrev']; ?></td>
-                <td><?php echo $donnees['AchatTot']." €"; $totAT += $donnees['AchatTot']; ?></td>
-                <td><?php echo $donnees['EcartAch']." €"; $totEA += $donnees['EcartAch']; ?></td>
+                <td><?php if(!empty($donnees['AchatTot'])){ echo $donnees['AchatTot']." €"; $totAT += $donnees['AchatTot'];} else echo "0 €"; ?></td>
+                <td><?php if(!empty($donnees['EcartAch'])){ echo $donnees['EcartAch']." €"; $totEA += $donnees['EcartAch'];} else echo "0 €"; ?></td>
                 <td><?php echo $donnees['CHA_HeuresPrev']; $totHP += $donnees['CHA_HeuresPrev']; ?></td>
                 <td><?php echo $donnees['HeureTot']; $totHT += $donnees['HeureTot']; ?></td>
                 <td><?php echo $donnees['EcartHeure']; $totEH += $donnees['EcartHeure']; ?></td>
