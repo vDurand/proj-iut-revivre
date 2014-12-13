f1e91c9219929a295a0c25a413d449ae09d1dee4
Pierre Friboulet <kepha76@voila.fr>
2014-12-13 04:53:10 +0100

    Tri alphabétique sur les chantiers
    Le CSS est à réparer

--- v2.0/oldWorks.php
+++ v2.0/oldWorks.php
@@ -1,77 +1,229 @@
 <?php
 $pageTitle = "Chantiers Archivés";
-	include('bandeau.php');
+include('bandeau.php');
 ?>
-		<script src="js/sorttable.js"></script>
-		<script>
-        $(document).ready(function() {
+    <script src="js/sorttable.js"></script>
+    <script>
+        $(document).ready(function () {
             $('.tooltip').tooltipster();
         });
-    	</script>
-		<div id="corps">
-			<div id="labelT">     
-				<label>Liste des Chantiers Archivés</label>
-			</div><br>
-				<div class="listeMembers">
-					<table class="sortable" cellpadding="5">
-						<thead>
-						<tr>
-							<td class="firstCol" style="text-align: center; width: 40px;">
-								<a>#</a>
-							</td>
-							<td style="text-align: center; width: 231px;">
-								<a>Chantier</a>
-							</td>
-							<td style="text-align: center; width: 155px;">
-								<a>Client</a>
-							</td>
-							<td style="text-align: center; width: 155px;">
-								<a>Responsable</a>
-							</td>
-							<td style="text-align: center; width: 155px; cursor: help;" class="sorttable_nosort tooltip" title="Classement par défaut.">
-								Debut
-							</td>
-							<td style="text-align: center; width: 155px; cursor: help;" class="sorttable_nosort tooltip" title="Vous ne pouvez pas classer par date de fin.">
-								<a>Fin</a>
-							</td>
-						</tr>
-					</thead>
-					<tbody>
-<?php
-	$sorter = 'CHA_DateDebut';
+    </script>
+    <div id="corps">
+    <div id="labelT">
+        <label>Liste des Chantiers Archivés</label>
+    </div>
+    <br>
+    <form method="post" action="rangChantierOld.php" name="EditClient">
+        <table id="alphaL">
+            <tr>
+                <td>
+                    <div class="selectDrop">
+                        <select name="trieur">
+                            <option value="0">Client</option>
+                            <option value="1">Responsable</option>
+                        </select>
+                    </div>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="#" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="A" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="B" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="C" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="D" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="E" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="F" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="G" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="H" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="I" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="J" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="K" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="L" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="M" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="N" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="O" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="P" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="Q" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="R" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="S" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="T" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="U" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="V" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="W" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="X" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="Y" class="alphaButton">
+		              </span>
+                </td>
+                <td>
+		              <span>
+		                <input name="submit" type="submit" value="Z" class="alphaButton">
+		              </span>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <div class="listeMembers">
+        <table class="sortable" cellpadding="5">
+            <thead>
+            <tr>
+                <td class="firstCol" style="text-align: center; width: 40px;">
+                    <a>#</a>
+                </td>
+                <td style="text-align: center; width: 231px;">
+                    <a>Chantier</a>
+                </td>
+                <td style="text-align: center; width: 155px;">
+                    <a>Client</a>
+                </td>
+                <td style="text-align: center; width: 155px;">
+                    <a>Responsable</a>
+                </td>
+                <td style="text-align: center; width: 155px; cursor: help;" class="sorttable_nosort tooltip"
+                    title="Classement par défaut.">
+                    Debut
+                </td>
+                <td style="text-align: center; width: 155px; cursor: help;" class="sorttable_nosort tooltip"
+                    title="Vous ne pouvez pas classer par date de fin.">
+                    <a>Fin</a>
+                </td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $sorter = 'CHA_DateDebut';
 
-	$reponse = mysqli_query($db, "SELECT *, max(Id) FROM Chantiers ch JOIN ChantierClient vcl ON ch.CHA_NumDevis=vcl.CNumDevis LEFT JOIN ChantierResp vre ON ch.CHA_NumDevis=vre.RNumDevis LEFT JOIN ChantierEtat cet ON ch.CHA_NumDevis=NumDevis Group By NumDevis HAVING max(Id)>3 ORDER BY ch.CHA_DateDebut DESC");
-	while ($donnees = mysqli_fetch_assoc($reponse))
-	{
-?>
-						<form method="get" action="detailChantier.php" name="detailClient">
-							<input type="hidden" name="NumC" value="">
-							<tr onclick="javascript:submitViewDetail('<?php echo $donnees['CHA_NumDevis']; ?>', 'detailClient');" style="font-size: 14;">
-								<td><?php echo formatUP($donnees['CHA_NumDevis']); ?></td>
-								<td><?php echo formatLOW($donnees['CHA_Intitule']); ?></td>
-								<td><?php echo formatUP($donnees['Client']); ?> <?php echo formatLOW($donnees['ClientP']); ?></td>
-								<td><?php echo formatUP($donnees['Resp']); ?> <?php echo formatLOW($donnees['RespP']); ?></td>
-								<td><?php echo dater($donnees['CHA_DateDebut']); ?></td>
-								<td>
-<?php 
-		if ($donnees['max(Id)']==4) {
-			echo dater($donnees['CHA_DateFinReel']);
-		}
-		if ($donnees['max(Id)']==5) {
-		  	echo Refuse;
-		} 
-?>
-								</td>
-							</tr>
-						</form>
+            $reponse = mysqli_query($db, "SELECT * FROM ChantierMax Join TypeEtat ON IdMax=TYE_Id WHERE IdMax>3 ORDER BY CHA_DateDebut DESC");
+            while ($donnees = mysqli_fetch_assoc($reponse)) {
+                ?>
+                <form method="get" action="detailChantier.php" name="detailClient">
+                    <input type="hidden" name="NumC" value="">
+                    <tr onclick="javascript:submitViewDetail('<?php echo $donnees['CHA_NumDevis']; ?>', 'detailClient');"
+                        style="font-size: 14;">
+                        <td><?php echo formatUP($donnees['CHA_NumDevis']); ?></td>
+                        <td><?php echo formatLOW($donnees['CHA_Intitule']); ?></td>
+                        <td><?php echo formatUP($donnees['Client']); ?> <?php echo formatLOW($donnees['ClientP']); ?></td>
+                        <td><?php echo formatUP($donnees['Resp']); ?> <?php echo formatLOW($donnees['RespP']); ?></td>
+                        <td><?php echo dater($donnees['CHA_DateDebut']); ?></td>
+                        <td>
+                            <?php
+                            if ($donnees['max(Id)'] == 4) {
+                                echo dater($donnees['CHA_DateFinReel']);
+                            }
+                            if ($donnees['max(Id)'] == 5) {
+                                echo Refuse;
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                </form>
+            <?php
+            }
+            mysqli_free_result($reponse);
+            ?>
+            </tbody>
+        </table>
+    </div>
+    </div>
 <?php
-	}
-	mysqli_free_result($reponse);
-?>					
-					</tbody>
-				</table>
-			</div>
-		</div>
-<?php  
-	include('footer.php');
+include('footer.php');
 ?>
--- v2.0/rangChantierAwaiting.php
+++ v2.0/rangChantierAwaiting.php
@@ -179,34 +179,30 @@ include('bandeau.php');
                 <td style="text-align: center; width: 155px;">
                     <a>Responsable</a>
                 </td>
-                <td style="text-align: center; width: 155px; cursor: help;" class="sorttable_nosort tooltip"
-                    title="Classement par défaut.">
+                <td style="text-align: center; width: 155px;">
                     Debut
                 </td>
                 <td style="text-align: center; width: 155px;">
-                    <a>Etat</a>
+                    <a>Fin</a>
                 </td>
             </tr>
             </thead>
             <tbody>
             <?php
             $sorter = 'CHA_DateDebut';
+            $alpha = $_POST["submit"] . '%';
+            if ($_POST["trieur"] == 0) {
+                $sorter = 'Client';
+            }
+            if ($_POST["trieur"] == 1) {
+                $sorter = 'Resp';
+            }
+            if ($_POST["submit"] == '#') {
+                $reponse = mysqli_query($db, "SELECT * FROM ChantierMax Join TypeEtat ON IdMax=TYE_Id WHERE IdMax=1 ORDER BY CHA_DateDebut DESC");
+            } else {
+                $reponse = mysqli_query($db, "SELECT * FROM ChantierMax Join TypeEtat ON IdMax=TYE_Id WHERE $sorter like '$alpha' AND IdMax=1 ORDER BY $sorter");
+            }
 
-            /*CREATE OR REPLACE VIEW ChantierClient AS SELECT co.CHA_NumDevis, pe.PER_Nom as Client, pe.PER_Prenom as ClientP FROM Commanditer co JOIN Clients cl ON co.CLI_NumClient=cl.CLI_NumClient JOIN Personnes pe ON cl.PER_Num=pe.PER_Num;
-
-            CREATE OR REPLACE VIEW ChantierClient AS SELECT co.CHA_NumDevis as CNumDevis, pe.PER_Nom as Client, pe.PER_Prenom as ClientP, pe.PER_TelFixe as ClientTel, pe.PER_Email as ClientEmail, pe.PER_Adresse as ClientAd, pe.PER_Ville as ClientV, pe.PER_CodePostal as ClientCP FROM Commanditer co JOIN Clients cl ON co.CLI_NumClient=cl.CLI_NumClient JOIN Personnes pe ON cl.PER_Num=pe.PER_Num;
-
-            CREATE OR REPLACE VIEW ChantierResp AS SELECT en.CHA_NumDevis as RNumDevis, pe.PER_Nom as Resp, pe.PER_Prenom as RespP FROM Encadrer en JOIN Salaries sa ON en.SAL_NumSalarie=sa.SAL_NumSalarie JOIN Personnes pe ON sa.PER_Num=pe.PER_Num;*/
-
-            //$reponse = mysqli_query($db, "SELECT * FROM Chantiers ch JOIN ChantierClient vcl ON ch.CHA_NumDevis=vcl.CHA_NumDevis LEFT JOIN ChantierResp vre ON ch.CHA_NumDevis=vre.CHA_NumDevis ORDER BY ch.CHA_NumDevis DESC");
-
-            //SELECT * FROM ChantierEtat WHERE Id = (select max(Id) AS MAXId from ChantierEtat WHERE NumDevis=11)
-
-            //SELECT * FROM Chantiers ch JOIN ChantierClient vcl ON ch.CHA_NumDevis=vcl.CNumDevis LEFT JOIN ChantierResp vre ON ch.CHA_NumDevis=vre.RNumDevis LEFT JOIN ChantierEtat cet ON ch.CHA_NumDevis=NumDevis WHERE Id = (select NumDevis, max(Id) From ChantierEtat Group By NumDevis) ORDER BY ch.CHA_NumDevis DESC
-
-            //SELECT *, max(Id) FROM Chantiers ch JOIN ChantierClient vcl ON ch.CHA_NumDevis=vcl.CNumDevis LEFT JOIN ChantierResp vre ON ch.CHA_NumDevis=vre.RNumDevis LEFT JOIN ChantierEtat cet ON ch.CHA_NumDevis=NumDevis Group By NumDevis HAVING max(Id)>4 ORDER BY ch.CHA_NumDevis DESC
-
-            $reponse = mysqli_query($db, "SELECT * FROM ChantierMax Join TypeEtat ON IdMax=TYE_Id WHERE IdMax=1 ORDER BY CHA_DateDebut DESC");
             while ($donnees = mysqli_fetch_assoc($reponse)) {
                 ?>
                 <form method="get" action="detailChantier.php" name="detailClient">
@@ -218,7 +214,15 @@ include('bandeau.php');
                         <td><?php echo formatUP($donnees['Client']); ?> <?php echo formatLOW($donnees['ClientP']); ?></td>
                         <td><?php echo formatUP($donnees['Resp']); ?> <?php echo formatLOW($donnees['RespP']); ?></td>
                         <td><?php echo dater($donnees['CHA_DateDebut']); ?></td>
-                        <td><?php echo $donnees['TYE_Nom']; ?></td>
+                        <td>
+                            <?php
+                            if ($donnees['IdMax'] == 4) {
+                                echo dater($donnees['CHA_DateFinReel']);
+                            } else {
+                                echo $donnees['TYE_Nom'];
+                            }
+                            ?>
+                        </td>
                     </tr>
                 </form>
             <?php
