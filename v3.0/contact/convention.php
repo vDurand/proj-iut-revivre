518a6c9d97fabc6bb7703bef4cc30d17891cbe28
nOwXO <nowxosmail@gmail.com>
2015-06-02 23:40:03 +0200

    Correctifs Conventions

--- v3.0/contact/convention.php
+++ v3.0/contact/convention.php
@@ -5,100 +5,161 @@
 	$arrayElements = Array();
 ?>
 <div id="corps">
-	<form>
-		<table style="margin:10px auto 0 auto;" id="tableConvention">
-			<?php
-				$x=$max=0;
-				$flag = false;
-				$query = mysqli_query($db,"SELECT CNV_Id, CNV_Nom AS 'nom', CNV_Couleur as 'couleur' FROM convention WHERE CNV_Actif=true ORDER BY CNV_Id;");
-				while($data = mysqli_fetch_assoc($query))
-				{
-					$max = ($data["CNV_Id"] > $max) ? $data["CNV_Id"] : $max; 
-					echo '<tr id="tr'.$x.'">
-					<td><input type="text" id="nom'.$x.'" name="nom'.$x.'" style="color:'.$data["couleur"].';text-transform:uppercase;" value="'.$data["nom"].'" onchange="changeNom()"/></td>
-					<td><input name="coul'.$x.'" id="coul'.$x.'" type="color" value="'.$data["couleur"].'" onchange="changeCouleur('.$x.')"/></td>
-					<td><input name="suppr" type="button" class="delCross" value="x" onclick="delConvention('.$x.')"/></td>
-					</tr>';
-					
-					$arrayElements[$x++] = Array((int)($data["CNV_Id"]),0);
-				}
-			?>
-		</table>
-		<table style="margin:10px auto 0 auto;">
-			<tr>
-				<td>
-					<input name="nouvConv" id="NouvConv" type="button" class="buttonC" value="Nouvelle Conv" onclick="addConvention()">
-					<input name="cancel" id="cancel" type="button" class="buttonC" value="Annuler" disabled="disabled" onclick="if(confirm('Etes-vous sûr de vouloir annuler ?')){window.location.replace('./convention.php');}">
-				</td>
-				<td><input name="validConv" type="button" class="buttonC" value="Sauvegarder" onclick="postData()"></td>
-			</tr>
-		</table>
-	</form>
-	<form method="post" action="./post_convention.php" name="valid_convention" id="valid_convention">
-		<input type='hidden' id="Tableau" name='Tableau' value=''>
-		<input type='hidden' id="redirectPage" name='redirectPage' value="./convention.php">
-	</form>
+    <div id="labelT">     
+        <label>Gestion des conventions administratives</label>
+   	</div>
+    <br/>
+    <div style="width:100%; max-width:100%; background-color:white; box-shadow:1px 1px 3px #555; padding:15px 0px;">
+        <form>
+            <table style="margin:0 auto 0 auto; width:400px;">
+                <tr>
+                    <td colspan="2">
+                        <input name="nouvConv" id="nouvConv" type="button" class="buttonNormal" value="Ajouter une convention" onclick="addConvention()">
+                    </td>
+                    <td style="text-align:right;">
+                        <input name="cancel" id="cancel" type="button" class="buttonNormal" value="Annuler" disabled="disabled" 
+                               onclick="if(confirm('Etes-vous sûr de vouloir annuler ?')){window.location.replace('./convention.php');}">
+                        <input id="validConv" name="validConv" type="button" class="buttonNormal" disabled="disabled" value="Sauvegarder" onclick="postData()">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3"><hr/></td>
+                </tr>
+            </table>
+        </form>
+        <table style="margin:10px auto 0 auto;" id="tableConvention">
+            <?php
+                $x=$max=0;
+                $flag = false;
+                $query = mysqli_query($db,"SELECT CNV_Id, CNV_Nom AS 'nom', CNV_Couleur as 'couleur' FROM convention ORDER BY CNV_Id;");
+                while($data = mysqli_fetch_assoc($query))
+                {
+                    $max = ($data["CNV_Id"] > $max) ? $data["CNV_Id"] : $max;
+                    echo '<tr id="tr'.$data["CNV_Id"].'" style="height: 32px;">
+                            <td>
+                                <input type="text" id="nom'.$data["CNV_Id"].'" name="nom'.$data["CNV_Id"].'" style="color:'.$data["couleur"].'; 
+                                        text-transform:uppercase; height: 22px;" value="'.$data["nom"].'" onkeyup="changeNom()"/>
+                            </td>
+                            <td>
+                                <input name="coul'.$data["CNV_Id"].'" id="coul'.$data["CNV_Id"].'" type="color" style="margin: 0 5px;" value="'.$data["couleur"].'" onchange="changeCouleur('.$data["CNV_Id"].')"/>
+                            </td>
+                            <td>
+                                <input name="suppr" type="button" class="delCross" value="x" onclick="delConvention('.$data["CNV_Id"].')"/>
+                            </td>
+                        </tr>';
+                    $arrayElements[$x++] = Array((int)($data["CNV_Id"]),0);
+                }
+            ?>
+        </table>
+        <form method="post" action="./conventionPost.php" name="valid_convention" id="valid_convention">
+            <input type='hidden' id="Tableau" name='Tableau' value=''>
+            <input type='hidden' id="typeAction" name='typeAction' value=''>
+            <input type='hidden' id="redirectPage" name='redirectPage' value="./convention.php">
+        </form>
+    </div>
 </div>
 
 <script type="text/javascript">
 	<?php
 		$js_array = json_encode($arrayElements);
 		echo "var tableau = ".$js_array.";
-		var maxi = ".($max+1).";";
-		
+		var maxi = ".($max+1).";
+        var idRef = ".($max).";";
 	?>
-	
+    var changement = false;
+    
 	function changeNom()
 	{
+        changement = true;
 		document.getElementById("cancel").disabled = "";
+        document.getElementById("validConv").disabled = "";
 	}
-	
-	//Permet de changer la couleur du texte quand la couleur change
-	
+    
 	function changeCouleur(id)
 	{
+        changement = true;
 		document.getElementById("cancel").disabled = "";
+        document.getElementById("validConv").disabled = "";
 		document.getElementById("nom"+id).style.color = document.getElementById("coul"+id).value;
 	}
 	
 	function addConvention()
 	{
 		document.getElementById("cancel").disabled = "";
-		var nom = "nom"+tableau.length;
-		var coul = "coul"+tableau.length;
-		var suppr = "suppr"+tableau.length;
+        document.getElementById("validConv").disabled = "";
+		var nom = "nom"+maxi;
+		var coul = "coul"+maxi;
+		var suppr = "suppr"+maxi;
 		var table = document.getElementById('tableConvention');
 		var ligne = table.insertRow(tableau.length);
-		ligne.id="tr"+tableau.length;
-		table.rows[tableau.length].innerHTML = '<td>\
+		ligne.id="tr"+maxi;
+		ligne.innerHTML = '<td>\
 				<input type="text" id="'+nom+'" name="'+nom+'" style="color:#000000; text-transform:uppercase;" placeholder="nom" onchange="changeNom()"/>\
 			</td>\
 			<td>\
-				<input id="'+coul+'" name="'+coul+'" type="color" value="#000000" onchange="changeCouleur('+tableau.length+')"/>\
+				<input id="'+coul+'" name="'+coul+'" type="color" style="margin: 0 5px;" value="#000000" onchange="changeCouleur('+maxi+')"/>\
 			</td>\
 			<td>\
-				<input name="'+suppr+'" id="'+suppr+'" type="button" class="delCross" value="x" onclick="delConvention('+tableau.length+')">\
+				<input name="'+suppr+'" id="'+suppr+'" type="button" class="delCross" value="x" onclick="delConvention('+maxi+')">\
 			</td>';
+        ligne.style.height="32px";
 		tableau[tableau.length] = Array(maxi++,1);
 	}
 	
-	function delConvention(x){
-		var tr = document.getElementById('tr'+x);
-		tr.style.display = 'none';
-		tableau[x][1]=2;
+	function delConvention(x)
+    {
+        if(confirm("Êtes-vous sûr de vouloir supprimer cette convention ?"))
+        {
+            if(x <= idRef)
+            {
+                document.getElementById('typeAction').value = "delete";
+                document.getElementById('Tableau').value = JSON.stringify(Array(x, document.getElementById('nom'+x).value));
+                document.getElementById("valid_convention").submit();
+            }
+            else
+            {
+                element = document.getElementById('tr'+x);
+                element.parentNode.removeChild(element);
+                for(var y=0; y<tableau.length; y++)
+                {
+                    if(tableau[y][0] == x)
+                    {
+                        tableau.splice(y,1);
+                        break;
+                    }
+                }
+                if(idRef == tableau.length && changement == false)
+                {
+                    document.getElementById("cancel").disabled = "disabled";
+                    document.getElementById("validConv").disabled = "disabled";
+                }
+            }
+        }
 	}
 	
 	function postData()
 	{
+        erreur = true;
 		var tableauFinal = new Array;
 		for(var x=0; x<tableau.length; x++)
 		{
-			inputText = document.getElementById("nom"+x);
-			inputColor = document.getElementById("coul"+x);
-			tableauFinal[x] = new Array(tableau[x][0], inputText.value, inputColor.value, 1, tableau[x][1]);
+			inputText = document.getElementById("nom"+tableau[x][0]);
+			inputColor = document.getElementById("coul"+tableau[x][0]);
+            if(inputText.value != "")
+            {
+                tableauFinal[x] = new Array(tableau[x][0], inputText.value, inputColor.value, tableau[x][1]);
+                erreur = false;
+            }
+            else
+            {
+                alert("Veuillez remplir tous les noms de convention !");
+                erreur = true;
+                break;   
+            }
 		}
-		if(confirm("Etes-vous sûr de vouloir sauvegarder le planning ?"))
+		if(!erreur && confirm("Etes-vous sûr de vouloir sauvegarder le planning ?"))
 		{
+            document.getElementById('typeAction').value = "edit";
 			document.getElementById('Tableau').value = JSON.stringify(tableauFinal);
 		   	document.getElementById("valid_convention").submit();
 		}
---
+++ v3.0/contact/conventionPost.php
@@ -0,0 +1,114 @@
+<?php
+	$pageTitle = "Envoi des propriétés des conventions";
+	$pwd='../';
+	include($pwd.'bandeau.php');
+?>
+<div id="corps">
+<?php
+    if(isset($_POST['Tableau']) && isset($_POST['typeAction']))
+    {
+
+        $typeAction = $_POST['typeAction'];
+        $tableau = json_decode($_POST["Tableau"]);
+        if(mysqli_query($db, 'SET autocommit=0;') && mysqli_query($db, 'START TRANSACTION;'))
+        {
+            if($typeAction == "edit")
+            {
+                $erreur = false;	
+                for($x=0; $x<sizeof($tableau); $x++)
+                {
+                    if($tableau[$x][3]==1)
+                    {
+                        $query = mysqli_query($db, "INSERT INTO convention(CNV_Id, CNV_Nom, CNV_Couleur) VALUES (".($tableau[$x][0]).",upper('".$tableau[$x][1]."'),'".$tableau[$x][2]."');");
+                        if(!$query)
+                        {
+                            $erreur = true;
+                        }
+                    }
+                    else if($tableau[$x][3]==0)
+                    {
+                        $query = mysqli_query($db, "UPDATE convention SET CNV_Nom='".$tableau[$x][1]."', CNV_Couleur='".$tableau[$x][2]."' WHERE CNV_ID='".($tableau[$x][0])."';");
+                        if(!$query)
+                        {
+                            $erreur = true;
+                        }
+                    }
+                    else
+                    {
+                        $erreur = true;
+                    }
+                }
+                
+                if($erreur)
+                {
+                    mysqli_query($db, 'ROLLBACK;');
+                    echo '<div id="bad">
+                      <label>Une erreur s\'est produite lors de la sauvegarde des conventions !</label>
+                      </div>';
+                }
+                else
+                {
+                    mysqli_query($db, 'COMMIT;');
+                    echo '<div id="good">
+                        <label>La sauvegarde des conventions s\'est déroulée avec succès !</label>
+                        </div>';
+                }
+            }
+            elseif($typeAction == "delete")
+            {
+                $query = mysqli_query($db, "DELETE FROM convention WHERE CNV_Id=".($tableau[0]).";");
+                if(!$query)
+                {
+                    mysqli_query($db, 'ROLLBACK;');
+                    echo "<div id=\"bad\">
+                        <label>La convention ".$tableau[1]." ne peut pas être suprimée, des salariés y sont associés</label>
+                        </div>";
+                }
+                else
+                {
+                    mysqli_query($db, 'COMMIT;');
+                    echo '<div id="good">
+                      <label>La convention '.$tableau[1].' a été supprimée avec succès !</label>
+                      </div>';
+                }
+            }
+            else
+            {
+                echo '<div id="bad">
+                  <label>Une erreur s\'est produite lors de la sauvegarde des conventions !</label>
+                  </div>';
+            }
+        }
+        else
+        {
+            echo '<div id="bad">
+              <label>Une erreur s\'est produite lors de la sauvegarde des conventions !</label>
+              </div>';
+        }
+
+    }
+    else
+    {
+        echo '<div id="bad">
+          <label>Une erreur s\'est produite lors de la sauvegarde des conventions !</label>
+          </div>';
+    }
+
+	if(isset($_POST['redirectPage']))
+	{
+		$locationPath = $_POST['redirectPage'];
+	    echo '<script type="text/javascript">
+		window.setTimeout("location=(\''.$locationPath.'\');",2500);
+		</script>';
+	}
+	else
+	{
+		echo '<script type="text/javascript">
+		window.setTimeout("location=(\''.$pwd.'home.php\');",2500);
+		</script>';
+	}
+?>
+</div>
+<?php
+  	include($pwd.'footer.php');
+?>
